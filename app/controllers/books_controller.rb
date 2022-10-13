e35d523fab7c0b3b43d758d559e5fd48fb86e497
Zacharia <ndunguzachary24@gmail.com>
2022-10-13 11:27:36 +0300

    Authentication

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,7 @@
 class BooksController < ApplicationController
+    def index
+        book = Book.all 
+        render json: book
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+resources :books, only: [:index, :show, :delete, :update]
+resources :reviews, only: [:index, :show]
+resources :users, only: [:index, :show, :create]
+post "/login", to: "sessions#create"
+get "/me", to:  "users#show"
 end
