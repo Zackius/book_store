ae34cd38ae5e437c9e47d95a1fd7417ee6cfc63d
Zacharia <ndunguzachary24@gmail.com>
2022-10-13 13:09:41 +0300

    post creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,33 @@
 class BooksController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    # before_action  :authorize
+    # skip_before_action :authorize, only:  [:index]
+   
+     
+    def show
+book = Book.find(params[:id])
+render json: book
+    end
+ def create 
+    book = Book.create(book_params)
+    render json:  book,   status: :created
+
+ end 
     def index
         book = Book.all 
         render json: book
-
     end
+
+    private
+
+    def book_params
+params.permit(:title,  :image,  :author, :description, :price)
+    end
+    def render_not_found_response
+        render json: {error: "Book not found"},  status: :not_found
+    end
+
+#     def authorize
+# return render json: {error:  "Not authorized"}, status:  :unauthorized
+#     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-resources :books, only: [:index, :show, :delete, :update]
-resources :reviews, only: [:index, :show]
-resources :users, only: [:index, :show, :create]
-post "/login", to: "sessions#create"
-get "/me", to:  "users#show"
+
+resources :books, only: [:index, :create]
+# resources :reviews, only: [:index, :show]
+# resources :users, only: [:index, :show, :create]
+#  do
+# post "/login", to: "sessions#create"
+# get "/me", to:  "users#show"
+#  end
 end
